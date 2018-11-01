0cf7b002206a1cae871c12bca1ae76897fc65ca8
Matt Parker <matthewp14@gmail.com>
2018-11-01 14:58:28 +0100

    task 2 testing complete

--- src/testingHarness/Tester.java
+++ src/testingHarness/Tester.java
@@ -7,7 +7,7 @@ public class Tester {
 	//Probably looks like commented version on mac/linux 
 	//static String testPath = "../../tests/instructor/";
 	static String testPath = "tests/instructor/task2/";
-	static String testName = "branchcnt.bin";
+	static String testName = "branchmany.bin";
 	public static void main(String[] args) {
 		String[] arguments = new String[]{testPath + testName};
 		IsaSim.main(arguments);
